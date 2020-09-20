3c2adff66a0a949b7466b2bf73a2e9205cd98ca0
Moses <lazaromoses77@gmail.com>
2020-09-20 09:41:38 +0300

    Updates

---
+++ precache-manifest.6765cafdc751c9367b963379ea23acef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4fc80c54cdfcc796bf7ab315705026",
+    "url": "/weekendproj/index.html"
+  },
+  {
+    "revision": "9b58e3c2108a55cea4f8",
+    "url": "/weekendproj/static/css/main.f1816793.chunk.css"
+  },
+  {
+    "revision": "897fb1fc02204b468db5",
+    "url": "/weekendproj/static/js/2.0569a911.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weekendproj/static/js/2.0569a911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b58e3c2108a55cea4f8",
+    "url": "/weekendproj/static/js/main.0286f4f5.chunk.js"
+  },
+  {
+    "revision": "df0d48796ff7c65f2c93",
+    "url": "/weekendproj/static/js/runtime-main.c9cdaeff.js"
+  },
+  {
+    "revision": "9808a01ee66d4ffba1e60d6bffb35e2e",
+    "url": "/weekendproj/static/media/logo.9808a01e.png"
+  }
+]);
