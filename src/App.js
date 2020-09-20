c8419b4da2eda04d17d1a2aee8d4bf943458ba25
Moses <lazaromoses77@gmail.com>
2020-09-20 09:23:22 +0300

    changing router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from "./components/Home"
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 import Menu from "./components/menu"
 import './App.css';
 
